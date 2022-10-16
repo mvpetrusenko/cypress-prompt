6b8832613df8f00ed113dfa1fc593dbb87befc82
mvpetrusenko <70688011+mvpetrusenko@users.noreply.github.com>
2022-10-16 15:27:12 +0200

    Add files via upload

---
+++ startProject.js
@@ -0,0 +1,20 @@
+/*
+
+1)  in IDE create new project, for example, in PyCharm open terminal
+	in terminal type and click Enter button:
+	pwd (it will move to the present working directory) -> npm init
+	npm install cypress  (package manager of Node.js)
+	in your new project in PyCharm will be added package node_modules with files and files package.json and package-lock.json
+	npm -v (the actual cypress version will be shown)
+	node -v (the actual node.js version will be shown)
+
+2)  to open cypress project type in the OTHER terminal
+    npx cypress open
+    choose a browser to open cypress
+
+3) 	open site nodejs.org
+	download the newest version of node.js
+	install the downloaded program
+	again in the terminal of PyCharm type node -v to see that the version was changed to the newest
+
+*/
